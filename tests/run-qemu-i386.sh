cb17a1f3d7f9f5a05ea11e59a1ece8422e9bcaef
JJL772 <jeremy.lorelli.1337@gmail.com>
2025-03-11 19:27:10 -0700

    Fix network sharing on qemu; don't want to restrict the nic

--- tests/run-qemu-i386.sh
+++ tests/run-qemu-i386.sh
@@ -3,6 +3,9 @@
 set -e
 cd "$(dirname "${BASH_SOURCE[0]}")/../"
 
-KERNEL="build-cmake/build-rtems6-pc686-qemu/rtems-init"
+KERNEL="build-cmake/build-rtems6-pc686-qemu/rtems-init.exe"
 
-qemu-system-i386 -m 64 -no-reboot -serial mon:stdio -nographic -net nic,model=e1000 -net user,restrict=yes -append "--video=off --console=/dev/com1" $ARGS -kernel "$KERNEL" $@
+qemu-system-i386 -m 64 -no-reboot -serial mon:stdio -nographic -net nic,model=e1000 \
+    -net user \
+    -append "--video=off --console=/dev/com1 --mount=10.0.2.2:/home/jeremy/dev/epics:/epics --cwd=/home/jeremy/dev/epics/ioc/atlas/bin/RTEMS-pc686" \
+    $ARGS -kernel "$KERNEL" $@
