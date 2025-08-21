4cc29f143791330aaad452946128e233024905e7
Jeremy Lorelli <lorelli@slac.stanford.edu>
2025-08-20 17:54:06 -0700

    random work

--- rootfs/etc/init.d/S10modules.cmd
+++ rootfs/etc/init.d/S10modules.cmd
@@ -2,6 +2,7 @@
 # Load default modules for Cexpsh. Must be done after PATH
 #==========================================================#
 
+# Misc utilities (i.e. ls(), pwd())
 ld ("miscUtils.obj")
 
 # End of script.
